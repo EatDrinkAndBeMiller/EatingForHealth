613d129738c9d74cb9fc77f4167330a9dd5a6c1b
EatDrinkAndBeMiller <71196286+EatDrinkAndBeMiller@users.noreply.github.com>
2021-04-18 16:40:19 -0600

    welcome page changes

--- view/welcome.php
+++ view/welcome.php
@@ -1,52 +1,47 @@
 <?php include('header.php') ?>
 
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-  </ol>
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="images/download.jpeg" class="d-block w-100" alt="meal">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
+      <a href="diet.php">
+        <img src="images/welcome_carousel.png" class="d-block w-100" alt="welcome slide with link to The Diet">
+          <!-- <div class="carousel-caption d-none d-md-block">
+            <h5>title</h5>
+            <p>Caption</p>
+          </div> --></a>
     </div>
     <div class="carousel-item">
-      <img src="images/download1.jpeg" class="d-block w-100" alt="food">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
+      <a href="resources.php">
+        <img src="images/video_carousel.png" class="d-block w-100" alt="screenshot of tutorial video with link">
+          <!-- <div class="carousel-caption d-none d-md-block">
+            <h5>title</h5>
+            <p>Caption</p>
+          </div> --></a>
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
+    <a href="resources.php">
+        <img src="images/membership_carousel.png" class="d-block w-100" alt="slide about membership and link to sign up">
+          <!-- <div class="carousel-caption d-none d-md-block">
+            <h5>title</h5>
+            <p>Caption</p>
+          </div> --></a>
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+  <button class="carousel-control-prev" type="button" data-bs-target="##carouselExampleCaptions" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="##carouselExampleCaptions" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+    <span class="visually-hidden">Next</span>
+  </button>
 </div>
-  
+
   <br>
 <div class="container text-center">    
   <h3>Recommended Recipes</h3><br>
