a7b2de5794ab4861755c32c452657c7f5895d9b5
EatDrinkAndBeMiller <71196286+EatDrinkAndBeMiller@users.noreply.github.com>
2021-04-27 13:54:39 -0600

    update links & columns

--- view/substitutes.php
+++ view/substitutes.php
@@ -1,103 +1,115 @@
 <?php include('header.php')?>
   
 <div class="container text-center">    
-  <h3>Substitute Information</h3><br>
+  <h1 class="dark-text">Substitute Information</h1><br>
 </div>
   <div class="row">
-    <div class="col-3">
-      <img src="view/images/glutenfree.jpg" class="text-end" alt="Gluten Replacements" style="width:150px; float:right"> <br>
+    <div class="col-md-3 text-center">
+      <a href="index.php?action=sub-flours">
+      <img src="view/images/glutenfree.jpg" alt="Gluten Replacements"> 
+      </a><br>
     </div>
-    <div class="col-4">
-      <p class="text-left">Flour/Gluten Replacements</p>
-      <ul class="text-left">
-        <li>Almond Flour</li>
-        <li>Coconut Flour</li>
-        <li>Buckwheat Flour</li>
-        <li>Ground Golden Flaxseed</li>
-      </ul>
-    </div>
-    <div class="col-4">
-      <p class="text-left"></p><br>
-      <ul class="text-left">
-        <li>Brown Rice Flour</li>
-        <li>Garbanzo Flour</li>
-        <li>Quinoa Flour Flour</li>
-        <a href="index.php?action=sub-flours">Read More...</a><br>
-      </ul>
-    </div>
-  </div>
-
-<hr>
-
-  <div class="row">
-    <div class="col-3">
-      <img src="view/images/sugar.jpg" class="text-end" alt="Sugar Replacements" style="width:150px; float:right"> <br>
-    </div>
-    <div class="col-3">
-      <p class="text-left">Sugar/Sweetener Replacements</p>
-      <ul class="text-left">
-        <li>Fruit</li>
-        <li>Stevia</li>
-        <li>Honey</li>
-      </ul>
-    </div>
-    <div class="col-3">
-      <p class="text-left"></p><br>
-      <ul class="text-left">
-        <li>Maple Syrup</li>
-        <li>Erythritol</li>
-        <li>Xylitol</li>
-      </ul>
-    </div>
-    <div class="col-2">
-      <p class="text-left"></p><br>
-      <ul class="text-left">
-        <li>Monk Fruit</li>
-        <li>Swerve</li>
-        <a href=".?action=sub-sweeteners">Read More...</a>
-      </ul>
-    </div>
-  </div>
-<hr>
-
-
-  <div class="row">
-    <div class="col-3">
-      <img src="view/images/eggsubs.jpg" class="text-end" alt="Egg Replacements" style="width:150px; float:right"> <br>
-    </div>
-    <div class="col-3">
-      <p class="text-left">Egg Replacements</p>
-      <ul class="text-left">
-        <li>Aquafaba</li>
-        <li>Chia/Flaxseed Eggs</li>
-        <li>Carbonated Water</li>
-      </ul>
-    </div>
-    <div class="col-3">
-      <p class="text-left"></p><br>
-      <ul class="text-left">
-        <li>Nut Butter</li>
-        <li>Applesauce</li>
-        <li>Agar Powder</li>
-      </ul>
-    </div>
-    <div class="col-3">
-      <p class="text-left"></p><br>
-      <ul class="text-left">
-        <li>Banana</li>
-        <li>Yogurt</li>
-        <a href="index.php?action=sub-eggs">Read More...</a><br>
-      </ul>
+    <div class="col-md-8">
+      <h4 class="text-left dark-text"><a href="index.php?action=sub-flours">Flour/Gluten Replacements</a></h4>
+      <div class="row"> 
+        <div class="col-md-6">
+          <ul class="text-left">
+            <li>Almond Flour</li>
+            <li>Coconut Flour</li>
+            <li>Buckwheat Flour</li>
+            <li>Ground Golden Flaxseed</li>
+          </ul>
+        </div>
+        <div class="col-md-6">
+          <ul class="text-left">
+            <li>Brown Rice Flour</li>
+            <li>Garbanzo Flour</li>
+            <li>Quinoa Flour Flour</li>
+            <a href="index.php?action=sub-flours">Read More...</a><br>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 <hr>
 
   <div class="row">
-    <div class="col-3">
-      <img src="view/images/dairysub.jpg" class="text-end" alt="Gluten Replacements" style="width:150px; float:right"> <br>
+    <div class="col-md-3 text-center">
+      <a href=".?action=sub-sweeteners">
+      <img src="view/images/sugar.jpg" alt="Sugar Replacements"> 
+      </a><br>
     </div>
-    <div class="col-5">
-      <p class="text-left">Dairy Replacements</p>
+    <div class="col-md-8">
+      <h4 class="text-left dark-text"><a href=".?action=sub-sweeteners">Sugar/Sweetener Replacements</a></h4>
+      <div class="row"> 
+        <div class="col-md-4">
+        <ul class="text-left">
+          <li>Fruit</li>
+          <li>Stevia</li>
+          <li>Honey</li>
+        </ul>
+        </div>
+        <div class="col-md-4">
+          <ul class="text-left">
+            <li>Maple Syrup</li>
+            <li>Erythritol</li>
+            <li>Xylitol</li>
+          </ul>
+        </div>
+        <div class="col-md-4">
+          <ul class="text-left">
+            <li>Monk Fruit</li>
+            <li>Swerve</li>
+            <a href=".?action=sub-sweeteners">Read More...</a>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+<hr>
+
+
+  <div class="row">
+    <div class="col-md-3 text-center">
+      <a href="index.php?action=sub-eggs">
+      <img src="view/images/eggsubs.jpg" alt="Egg Replacements"> 
+      </a><br>
+    </div>
+    <div class="col-md-8">
+      <h4 class="text-left dark-text"><a href="index.php?action=sub-eggs">Egg Replacements</a></h4>
+        <div class="row"> 
+          <div class="col-md-4">
+            <ul class="text-left">
+              <li>Aquafaba</li>
+              <li>Chia/Flaxseed Eggs</li>
+              <li>Carbonated Water</li>
+            </ul>
+          </div>
+        <div class="col-md-4">
+          <ul class="text-left">
+            <li>Nut Butter</li>
+            <li>Applesauce</li>
+            <li>Agar Powder</li>
+          </ul>
+        </div>
+        <div class="col-md-4">
+          <ul class="text-left">
+            <li>Banana</li>
+            <li>Yogurt</li>
+            <a href="index.php?action=sub-eggs">Read More...</a><br>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+<hr>
+
+  <div class="row">
+    <div class="col-md-3 text-center">
+      <img src="view/images/dairysub.jpg" alt="Gluten Replacements"> <br>
+    </div>
+    <div class="col-md-5">
+      <h4 class="text-left text-dark">Dairy Replacements</h4>
       <ul class="text-left">
         <li>Canned Coconut Milk</li>
         <li>Store-bought dairy-free cream cheese*</li>
