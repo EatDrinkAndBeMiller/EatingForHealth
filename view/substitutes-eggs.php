40aae3267a88d3301602f574de7d191791bf859c
tsfollett <tsfollett@mail.fhsu.edu>
2021-04-27 18:04:22 -0600

    Update substitutes-eggs.php

--- view/substitutes-eggs.php
+++ view/substitutes-eggs.php
@@ -1,7 +1,38 @@
 <?php include('header.php')?>
   
 <div class="container text-center">    
-  <h3>Add content from eggs page on Google Drive</h3><br>
+  <h3><b><u>Egg Substitutions</u></b></h3><br>
  <br>
+</div>
+<div class="row">
+    <div class="col-md-3 text-center">
+        <img src="view/images/Egg-Substitutions-2.jpg" alt="Egg Substitutions" style="height:450px"> 
+      </a><br>
+      <small class="text-muted">Image&colon; <a href="https://www.thekitchenwhisperer.net/2014/07/31/egg-substitutes-cooking-baking/" target="_blank">The Kitchen Whisperer</a></small>
+    </div>
+    <div class="col-md-8">
+    
+    <p>There are many substitutes for eggs in cooking and baking. Which one to use depends on what you are making.<br><br>
+
+      This article does a great job explaining when to use each of the substitutes listed below: <a href="https://www.healthline.com/nutrition/egg-substitutes" target="_blank">13 Effective Substitutes for Eggs.</a><br><br>
+
+      <ul class="text-left">
+    <li>Applesauce</li>
+    <li>Mashed banana</li>
+    <li>Ground flaxseeds or chia seeds</li>
+    <li>Commercial egg replacer (not usable in phase 1; be sure to check the ingredients!)</li>
+    <li>Silken tofu (discuss using this with Dr. Shannon)</li>
+    <li>Vinegar and baking soda</li>
+    <li>Yogurt or buttermilk (not usable in phase 1)</li>
+    <li>Arrowroot powder (this is high in starch and can spike blood sugar)</li>
+    <li>Aquafaba (liquid in canned chickpeas)</li>
+    <li>Nut butter</li>
+    <li>Carbonated water</li>
+    <li>Agar-Agar or gelatin</li>
+    <li>Soy Lecithin (only use if you can have soy)</li>
+  </ul>
+  Pro Tip: When searching for recipes on the web, add “vegan” to avoid eggs (this also avoids dairy).
+    </div>
+  </div>
 </div>
 <?php include('footer.php')?>
