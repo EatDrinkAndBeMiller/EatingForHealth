7a0000e7fc164ff1bdcf34774c6b8e737acccd4e
EatDrinkAndBeMiller <71196286+EatDrinkAndBeMiller@users.noreply.github.com>
2021-04-29 10:08:09 -0600

    updating single recipe

--- view/single_recipe.php
+++ view/single_recipe.php
@@ -9,39 +9,58 @@ $img = Recipe::get_image($id);
 ?>
 
 <main>
-  <h1 style="text-align: center;"><?php echo $recipe['recipe_name']; ?></h1>
-    <?php echo '<img src="images/recipes/' . $img['recipe_image'] . '">' ?>
-
-<label>Prep Time (Minutes):</label><?php echo " ".$recipe['prep_time']." "; ?>
-    <label>Cook/Fridge Time (Minutes):</label><?php echo " ".$recipe['cook_time']." "; ?>
-    <label>Total Time (Minutes):</label><?php echo " ".$recipe['total_time']." "; ?>
-
-<div id="column1" style="float:left; width:23%; padding: 0 10px;">
-<h2 style="text-align: center;">Ingredients</h2>
-    <ul>
+<br><h1 style="text-align: center;"><?php echo $recipe['recipe_name']; ?></h1><br>
+  <div class="row justify-content-md-center">
+    <div class="col-md-3">
+      <div class="align-middle">
+        <?php echo '<img src="images/recipes/' . $img['recipe_image'] . '">' ?><br>
+        <?php if($img['recipe_caption'] != '') { ?>
+            <small class="text-muted vertical-center"><?php echo $img['recipe_caption']?></small>
+        <?php } ?>
+      </div>
+    </div>
+    <div class="col-md-7">
+      <div class="card bg-light">
+        <div class="card-body text-center">
+          <p><b>Prep Time:</b><?php echo " ".$recipe['prep_time']." "; ?> &nbsp; | &nbsp;
+            <b>Cook or Fridge Time:</b><?php echo " ".$recipe['cook_time']." "; ?> &nbsp; | &nbsp;
+            <b>Total Time:</b><?php echo " ".$recipe['total_time']." "; ?>
+        </div>
+      </div>
+      <h4>Ingredients</h4>
+        <ul>
         <?php foreach($ingredients as $i) : ?> 
             <li><?php $formatted = $i["measurement_qty"]." ".$i["measurement"]." ".$i["ingredient_name"]; 
                 echo $formatted; ?></li>
         <?php endforeach; ?>
-    </ul>
-</div>
-
-<div id="column2" style="float:left; width:auto; padding: 10 20px;">
-<h2 style="text-align: center;">Directions</h2>
-    <ol>
-        <li><?php if($recipe['step_a'] != '') { echo $recipe['step_a']; } ?></li>
-        <li><?php if($recipe['step_b'] != '') { echo $recipe['step_b']; } ?></li>
-        <li><?php if($recipe['step_c'] != '') { echo $recipe['step_c']; } ?></li>
-        <li><?php if($recipe['step_d'] != '') { echo $recipe['step_d']; } ?></li>
-    </ol>
-</div>
-
-<?php if($recipe['note'] != '') { ?>
-    <div id="column3" style="float:left; width:auto; padding: 10 20px;">
-        <h3 style="text-align: center;">Notes</h3>
-        <p><?php= $recipe['note'] ?></p>
+        </ul>
     </div>
-<?php } ?>
+  </div>
+
+  <div class="row justify-content-md-center">
+    <div class="col-md-10">
+        <h4 class="text-left">Directions</h4>
+            <ol>
+                <li><?php if($recipe['step_a'] != '') { echo $recipe['step_a']; } ?></li>
+                <?php if($recipe['step_b'] != '') { ?>
+                    <li><?php echo $recipe['step_b']; ?></li>
+                <?php } ?>
+                <?php if($recipe['step_c'] != '') { ?>
+                    <li><?php echo $recipe['step_c']; ?></li>
+                <?php } ?>
+                <?php if($recipe['step_d'] != '') { ?>
+                    <li><?php echo $recipe['step_d']; ?></li>
+                <?php } ?>
+            </ol>
+
+        <?php if($recipe['note'] != '') { ?>
+            <h6>Notes&colon;</h6>
+                <p><?php echo $recipe['note'] ?></p>
+        <?php } ?>
+
+        <p class="text-muted">Servings&colon; <?php echo $recipe['serving']?></p>
+    </div>
+  </div>
 
 </main><br><br><br>
 <?php include('footer2.php'); ?>
